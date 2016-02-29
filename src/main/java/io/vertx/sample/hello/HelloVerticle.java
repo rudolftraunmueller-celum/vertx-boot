8a1942f79ccf4a76af42cc4b48555eca8bc8c5b4
Rudolf Traunmüller <rudolf.traunmueller@celum.com>
2016-02-29 08:38:39 +0100

    events are now published instead of sent

--- src/main/java/io/vertx/sample/hello/HelloVerticle.java
+++ src/main/java/io/vertx/sample/hello/HelloVerticle.java
@@ -10,18 +10,11 @@ public class HelloVerticle extends AbstractVerticle {
   @Override
   public void start() throws Exception {
     String name = ManagementFactory.getRuntimeMXBean().getName();
-    vertx.createHttpServer().requestHandler(request -> {
-      request.response().end("Hello Java world !");
-    }).listen(8080);
     vertx.setPeriodic(1000, id -> {
-      vertx.eventBus().send("ping", "ping", reply -> {
-        if (reply.succeeded()) {
-          System.out.println("Reply: " + reply.result().body() + " " + new Date());
-        }
-      });
+      vertx.eventBus().publish("ping", "ping from " + name + " " + new Date());
     });
     vertx.eventBus().consumer("ping", message -> {
-      message.reply(message.body() + " from " + name);
+      System.out.println(message.body());
     });
   }
 }
